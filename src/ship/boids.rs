e06ca92a95eccfad46f2b2066a8262dc04c584bb
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-05-28 23:54:31 +0200

    feat(boid): add cohesion system

--- src/ship/boids.rs
+++ src/ship/boids.rs
@@ -97,11 +97,48 @@ fn alignment(
     }
 }
 
+fn cohesion(
+    system: Res<BoidSystem>,
+    time: Res<Time>,
+    mut query: Query<(Entity, &Transform, &mut Ship), With<Childship>>,
+) {
+    let range_squared = system.cohesion_settings.range * system.cohesion_settings.range;
+
+    // Center of mass of the nearby boids of every boid.
+    let mut com = HashMap::<Entity, (usize, Vec2)>::new();
+
+    let mut combinations = query.iter_combinations_mut();
+    while let Some([(a_entity, a_transform, a_ship), (_, b_transform, b_ship)]) =
+        combinations.fetch_next()
+    {
+        if a_transform
+            .translation
+            .distance_squared(b_transform.translation)
+            < range_squared
+            && a_ship.speed.dot(b_ship.speed) > -system.field_of_view
+        {
+            let (ref mut count, ref mut total) = com.entry(a_entity).or_insert((0, Vec2::ZERO));
+
+            *count += 1;
+            *total += b_transform.translation.truncate();
+        }
+    }
+
+    for (entity, transform, mut ship) in query.iter_mut() {
+        if let Some((count, total_pos)) = com.get(&entity) {
+            let com = *total_pos / *count as f32;
+            let to_com = com - transform.translation.truncate();
+            ship.speed += to_com / 10. * time.delta_seconds() * system.cohesion_settings.force;
+        }
+    }
+}
+
 pub struct BoidPlugin;
 impl Plugin for BoidPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(BoidSystem::default())
             .add_system(separation)
-            .add_system(alignment);
+            .add_system(alignment)
+            .add_system(cohesion);
     }
 }
